81af9920d7f329ee628806156b892ef65d1e93b1
Kavita Varshney <kavigupta96@gmail.com>
2018-07-06 23:18:00 +0530

    day5

---
+++ workspace/day5/TMatrix/WebContent/file.js
@@ -0,0 +1,15 @@
+function transposeArray(array, arrayLength){
+    var newArray = [];
+    for(var i = 0; i < array.length; i++){
+        newArray.push([]);
+    };
+
+    for(var i = 0; i < array.length; i++){
+        for(var j = 0; j < arrayLength; j++){
+            newArray[j].push(array[i][j]);
+        };
+    };
+
+    return newArray;
+}
+console.log(transposeArray([[1,2,3],[4,5,6],[7,8,9]],3));

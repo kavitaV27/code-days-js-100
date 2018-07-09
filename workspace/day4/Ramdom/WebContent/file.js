c0743f2f1b5acde7ca42e81f6afff1676a141896
Kavita Varshney <kavigupta96@gmail.com>
2018-07-09 22:14:48 +0530

    updated

--- workspace/day4/Ramdom/WebContent/file.js
+++ workspace/day4/Ramdom/WebContent/file.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var readline = require('readline');
 var r = readline.createInterface({
   input: process.stdin,
@@ -33,4 +34,41 @@ var arr = [10,20,30,22,45,84,2,4,56,8,79];
 });
 };
 
+=======
+var readline = require('readline');
+var r = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+var i = 0;
+var j = i+1;
+var random = function () {
+var arr = [10,20,30,22,45,84,2,4,56,8,79];
+    
+    var result1 = 0;
+    var result2 = 0;
+  	console.log("  answer")
+
+  r.question("sum of "+arr[i]+" and "+arr[j]+ "\n" , function (answer1) {
+
+    result1 = arr[i]+arr[j];
+    if (answer1 == result1)
+    {
+       console.log("correct");
+       i = i+1;
+       if(i>9)
+        return r.close();
+    }
+    else
+	{
+	  console.log("please try again");
+	}           
+	//log('Got it! Your answer was: "', answer, '"');
+    random();
+     
+});
+};
+
+>>>>>>> 66ecea0febc62e1fe72443f1b10027b4a34156f0
 random();

3d426b86f56af4d39883ab473372ecb433886f72
Kavita Varshney <40734073+kavitaV27@users.noreply.github.com>
2018-07-13 23:02:35 +0530

    day12

---
+++ Convert/WebContent/IntegerToOctal.js
@@ -0,0 +1,5 @@
+ var decimalNumber = 10; 
+  var octalNumber = decimalNumber.toString(8); 
+ console.log("Octal Number is " + octalNumber);
+ var binaryNumber = decimalNumber.toString(2); 
+ console.log("Binary Number is " + binaryNumber);

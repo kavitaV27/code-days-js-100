98173c0893636f715de0fa91b020fa0fdf879112
Kavita Varshney <40734073+kavitaV27@users.noreply.github.com>
2018-07-22 23:35:56 +0530

    day18

---
+++ workspace/Slargest/WebContent/slargest.js
@@ -0,0 +1,21 @@
+function Second_Greatest_Lowest(arr_num)
+{
+  arr_num.sort(function(x,y)
+           {
+           return x-y;
+           });
+  var uniqa = [arr_num[0]];
+  var result = [];
+  
+  for(var j=1; j < arr_num.length; j++)
+  {
+    if(arr_num[j-1] !== arr_num[j])
+    {
+      uniqa.push(arr_num[j]);
+    }
+  }
+    result.push(uniqa[1],uniqa[uniqa.length-2]);
+  return result.join(',');
+  }
+
+console.log(Second_Greatest_Lowest([5,6,7,2,1,9,3]));
